0b9119642f6859e9d04ee843cbf5055baed8b89c
Konkitoman <konkitoman@users.noreply.github.com>
2024-12-05 15:49:09 +0200

    Finish Day 4 Part 2

--- lib/day4.ex
+++ lib/day4.ex
@@ -4,11 +4,15 @@ defmodule Day4 do
     text = File.read!("in/day4-sample.txt")
     IO.puts("Part1:\n")
     part1(text)
+    IO.puts("Part2:\n")
+    part2(text)
 
     text = File.read!("in/day4.txt")
     IO.puts("\nResults\n")
     IO.puts("Part1:\n")
     part1(text)
+    IO.puts("Part2:\n")
+    part2(text)
   end
 
   def extract(text) do
@@ -70,4 +74,42 @@ defmodule Day4 do
   def find1(<<"XMAS", tail::binary>>), do: 1 + find1(tail)
   def find1(<<_::8, tail::binary>>), do: find1(tail)
   def find1(<<>>), do: 0
+
+  def part2(text) do
+    lines = text |> String.split("\n") |> Enum.filter(fn x -> x != "" end) |> Enum.to_list()
+
+    width = String.length(Enum.at(lines, 0)) - 1
+    height = Enum.count(lines) - 1
+
+    dbg(
+      Enum.map(1..(height - 1), fn y ->
+        Enum.map(1..(width - 1), fn x ->
+          get(lines, x, y) == "A" and
+            (
+              a = get(lines, x - 1, y - 1)
+              b = get(lines, x + 1, y + 1)
+              c = get(lines, x + 1, y - 1)
+              d = get(lines, x - 1, y + 1)
+
+              ((a == "M" and b == "S") or
+                 (a == "S" and b == "M")) and
+                ((c == "M" and d == "S") or
+                   (c == "S" and d == "M"))
+            )
+        end)
+        |> Enum.reduce(0, fn x, acc ->
+          if x do
+            acc + 1
+          else
+            acc
+          end
+        end)
+      end)
+      |> Enum.sum()
+    )
+  end
+
+  defp get(list, x, y) do
+    String.at(Enum.at(list, y), x)
+  end
 end
