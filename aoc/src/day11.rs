2300e7a99a478f7bd556a19a2835caf4e2abc5d5
Konkitoman <konkitoman@users.noreply.github.com>
2024-12-14 02:21:33 +0200

    Finish Day 11 Part 2

--- aoc/src/day11.rs
+++ aoc/src/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use aoc::Solution;
 
 pub struct Day11;
@@ -21,23 +23,49 @@ impl TDigits for usize {
 }
 
 fn simulate_plutonian_pebbles(nums: &mut Vec<usize>) {
-    let mut i = 0;
-    while i < nums.len() {
-        let num = &mut nums[i];
-        if *num == 0 {
-            *num = 1
+    let mut new_nums = Vec::with_capacity(nums.len() * 2);
+    for num in nums.drain(..) {
+        if num == 0 {
+            new_nums.push(1);
         } else if num.digits() % 2 == 0 {
-            let left = *num / (10usize.pow((num.digits() / 2) as u32));
-            let right = *num % (10usize.pow((num.digits() / 2) as u32));
+            let left = num / (10usize.pow((num.digits() / 2) as u32));
+            let right = num % (10usize.pow((num.digits() / 2) as u32));
 
-            *num = left;
-            nums.insert(i + 1, right);
-            i += 1;
+            new_nums.push(left);
+            new_nums.push(right);
         } else {
-            *num *= 2024;
+            new_nums.push(num * 2024);
         }
-        i += 1;
     }
+    *nums = new_nums;
+}
+
+fn simulate_plutonian_pebbles_len(
+    cache: &mut HashMap<(usize, usize), usize>,
+    num: usize,
+    steps: usize,
+) -> usize {
+    if let Some(res) = cache.get(&(num, steps)) {
+        return *res;
+    }
+
+    if steps == 0 {
+        return 1;
+    }
+
+    let res = if num == 0 {
+        simulate_plutonian_pebbles_len(cache, 1, steps - 1)
+    } else if num.digits() % 2 == 0 {
+        let left = num / (10usize.pow((num.digits() / 2) as u32));
+        let right = num % (10usize.pow((num.digits() / 2) as u32));
+        simulate_plutonian_pebbles_len(cache, left, steps - 1)
+            + simulate_plutonian_pebbles_len(cache, right, steps - 1)
+    } else {
+        simulate_plutonian_pebbles_len(cache, num * 2024, steps - 1)
+    };
+
+    cache.insert((num, steps), res);
+    res
 }
 
 impl Solution for Day11 {
@@ -56,5 +84,18 @@ impl Solution for Day11 {
         }
 
         numbers.len() as isize
+    }
+
+    fn part2(&self, input: &str) -> isize {
+        let numbers = input
+            .split(' ')
+            .map(|x| x.trim().parse::<usize>().unwrap())
+            .collect::<Vec<_>>();
+
+        let mut cache = HashMap::default();
+        numbers
+            .iter()
+            .map(|num| simulate_plutonian_pebbles_len(&mut cache, *num, 75))
+            .sum::<usize>() as isize
     }
 }
