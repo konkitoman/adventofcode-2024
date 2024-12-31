fc925913c8857ba6aa9f80361d1be98ac2198990
Konkitoman <konkitoman@users.noreply.github.com>
2024-12-31 23:48:03 +0200

    Finish Day 12 Part 2

--- aoc/src/day12.rs
+++ aoc/src/day12.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-use aoc::{BColor8, FColor256, FColor8, Solution};
+use aoc::{BColor256, BColor8, FColor256, FColor8, Solution};
 
 pub struct Day12;
 
@@ -100,7 +100,7 @@ impl World {
 
             if !finished {
                 #[cfg(feature = "vizualize")]
-                print_region(current.0, region, &self.data);
+                display_region(current.0, region, &self.data);
                 queue.sort();
                 queue.dedup();
                 queue.retain(|(x, y)| {
@@ -134,7 +134,7 @@ impl World {
             }
 
             #[cfg(feature = "vizualize")]
-            print_region(current.0, region, &self.data);
+            display_region(current.0, region, &self.data);
 
             o_current = Some(current);
         }
@@ -162,7 +162,24 @@ impl World {
     }
 }
 
-fn print_region(ch: char, region: &Region, map: &[Vec<char>]) {
+fn display_inside(map: &[Vec<char>], func: impl Fn((usize, usize)) -> (Option<u8>, Option<u8>)) {
+    for (y, row) in map.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            let res = func((x, y));
+            if let Some(f) = res.0 {
+                eprint!("{}", FColor256(f));
+            }
+            if let Some(b) = res.1 {
+                eprint!("{}", BColor256(b));
+            }
+            eprint!("{cell}");
+            eprint!("{}{}", FColor8::Default, BColor8::Default);
+        }
+        eprintln!();
+    }
+}
+
+fn display_region(ch: char, region: &Region, map: &[Vec<char>]) {
     eprintln!("{}:", ch);
     for (y, row) in map.iter().enumerate() {
         for (x, cell) in row.iter().enumerate() {
@@ -236,6 +253,210 @@ impl Solution for Day12 {
             }
             #[cfg(feature = "vizualize")]
             eprintln!()
+        }
+
+        sum as isize
+    }
+
+    fn part2(&self, input: &str) -> isize {
+        let mut world = World::new(input);
+        let plots = world.get_plots();
+
+        {
+            let line = (0..world.data[0].len()).map(|_| '.').collect::<Vec<_>>();
+            world.data.push(line.clone());
+            world.data.insert(0, line);
+
+            for row in world.data.iter_mut() {
+                row.insert(0, '.');
+                row.push('.');
+            }
+        }
+
+        let mut sum = 0;
+
+        fn print(world: &World, region: &Region, p: (usize, usize)) {
+            display_inside(&world.data, |pos| {
+                let b = region.contains(pos).then_some(1);
+                if pos == p {
+                    (Some(2), b)
+                } else {
+                    (None, b)
+                }
+            });
+        }
+
+        for mut plot in plots {
+            for region in plot.1.regions.iter_mut() {
+                region.poses = region
+                    .poses
+                    .iter()
+                    .map(|(x, y)| (x + 1, y + 1))
+                    .collect::<HashSet<_>>();
+
+                let mut edge = HashSet::<(usize, usize)>::default();
+
+                for (x, y) in region.poses.iter().copied() {
+                    for (ox, oy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
+                        let x = (x as isize + ox) as usize;
+                        let y = (y as isize + oy) as usize;
+
+                        if !region.poses.contains(&(x, y)) {
+                            edge.insert((x, y));
+                        }
+                    }
+                }
+
+                #[derive(Clone, Copy, Debug, PartialEq)]
+                enum Direction {
+                    Top,
+                    Right,
+                    Down,
+                    Left,
+                }
+
+                impl Direction {
+                    fn rotate_right(&self) -> Self {
+                        match self {
+                            Direction::Top => Self::Right,
+                            Direction::Right => Self::Down,
+                            Direction::Down => Self::Left,
+                            Direction::Left => Self::Top,
+                        }
+                    }
+
+                    fn rotate_left(&self) -> Self {
+                        match self {
+                            Direction::Top => Self::Left,
+                            Direction::Right => Self::Top,
+                            Direction::Down => Self::Right,
+                            Direction::Left => Self::Down,
+                        }
+                    }
+                }
+
+                impl std::ops::Add<(usize, usize)> for Direction {
+                    type Output = Option<(usize, usize)>;
+
+                    fn add(self, (x, y): (usize, usize)) -> Self::Output {
+                        Some(match self {
+                            Direction::Top => {
+                                if y == 0 {
+                                    return None;
+                                } else {
+                                    (x, y - 1)
+                                }
+                            }
+                            Direction::Right => (x + 1, y),
+                            Direction::Down => (x, y + 1),
+                            Direction::Left => {
+                                if x == 0 {
+                                    return None;
+                                } else {
+                                    (x - 1, y)
+                                }
+                            }
+                        })
+                    }
+                }
+
+                let mut walked = Vec::default();
+                let mut pos = edge.iter().max().copied().unwrap();
+                let mut direction = Direction::Top;
+                let mut i = 0;
+                let mut edges = 0;
+                loop {
+                    if i > 4 {
+                        break;
+                    }
+                    if let Some(next) = direction + pos {
+                        if !region.contains(next)
+                            && region.contains((direction.rotate_right() + pos).unwrap())
+                        {
+                            break;
+                        }
+                    };
+                    direction = direction.rotate_right();
+                    i += 1;
+                }
+                let mut o_current = Some((pos, direction));
+
+                i = 0;
+                while let Some(((x, y), direction)) = o_current.take() {
+                    i += 1;
+
+                    if walked.contains(&((x, y), direction)) {
+                        let out = edge
+                            .iter()
+                            .find(|pos| !walked.iter().map(|(x, _)| x).any(|p| &p == pos));
+                        if let Some(out) = out {
+                            i = 0;
+                            pos = *out;
+                            let mut direction = Direction::Top;
+                            loop {
+                                if i > 4 {
+                                    break;
+                                }
+                                if let Some(next) = direction + pos {
+                                    if !region.contains(next)
+                                        && region
+                                            .contains((direction.rotate_right() + pos).unwrap())
+                                    {
+                                        break;
+                                    }
+                                };
+                                direction = direction.rotate_right();
+                                i += 1;
+                            }
+                            o_current = Some((pos, direction));
+                            continue;
+                        }
+                        break;
+                    }
+
+                    walked.push(((x, y), direction));
+
+                    let (nx, ny) = (direction + (x, y)).unwrap();
+                    let (urx, ury) = (direction.rotate_right() + (nx, ny)).unwrap();
+                    #[cfg(feature = "vizualize")]
+                    {
+                        if i != 1 {
+                            eprintln!("\x1B[{}A", world.data.len() + 1)
+                        }
+                        display_inside(&world.data, |(_x, _y)| {
+                            let b = region.contains((_x, _y)).then_some(1);
+                            if _x == x && _y == y {
+                                (Some(2), b)
+                            } else if (_x, _y) == (nx, ny) {
+                                (Some(3), b)
+                            } else if (_x, _y) == (urx, ury) {
+                                (Some(6), b)
+                            } else {
+                                (None, b)
+                            }
+                        });
+                    }
+
+                    if region.contains((nx, ny)) {
+                        o_current = Some(((x, y), direction.rotate_left()));
+                        edges += 1;
+                    } else if !region.contains((urx, ury)) {
+                        o_current = Some(((nx, ny), direction.rotate_right()));
+                        edges += 1;
+                    } else {
+                        o_current = Some(((nx, ny), direction));
+                    }
+
+                    #[cfg(feature = "vizualize")]
+                    std::thread::sleep(std::time::Duration::from_millis(100));
+                }
+
+                eprintln!("Edges: {edges}\n");
+                sum += edges * region.poses.len();
+
+                #[cfg(feature = "vizualize")]
+                eprintln!();
+            }
         }
 
         sum as isize
